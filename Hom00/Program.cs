5cb4b9bca67c1bf9eebc43a68c9ed8f9dce23611
Toro_rt49@itstepru.onmicrosoft.com <Toro_rt49@itstepru.onmicrosoft.com>
2022-03-14 00:21:05 +0700

    1

--- Hom00/Program.cs
+++ Hom00/Program.cs
@@ -129,35 +129,35 @@ namespace homework
         }
         static void Method5()
         {
-            Console.WriteLine("Введите номер месяца");
-            int month = int.Parse(Console.ReadLine());
+             Console.WriteLine("Введите номер месяца");
+             int month = int.Parse(Console.ReadLine());
 
-            switch (month)
-            {
-                case 12:
-                case 1:
-                case 2:
-                    Console.WriteLine("зима");
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    Console.WriteLine("весна");
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    Console.WriteLine("лето");
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    Console.WriteLine("осень");
-                    break;
-                default:
-                    Console.WriteLine("Неверный ввод");
-                    break;
-            }
+             switch (month)
+             {
+                 case 12:
+                 case 1:
+                 case 2:
+                     Console.WriteLine("зима");
+                     break;
+                 case 3:
+                 case 4:
+                 case 5:
+                     Console.WriteLine("весна");
+                     break;
+                 case 6:
+                 case 7:
+                 case 8:
+                     Console.WriteLine("лето");
+                     break;
+                 case 9:
+                 case 10:
+                 case 11:
+                     Console.WriteLine("осень");
+                     break;
+                 default:
+                     Console.WriteLine("Неверный ввод");
+                     break;
+             }
         }
         static void Method6()
         {
